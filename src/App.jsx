ede8174663e0d7e4d1f98adce1bef830d4404a43
Mayank <mayankkumardas12@gmail.com>
2025-03-17 20:53:59 +0530

    Shifts to components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Card from "./Card";
+import Card from "./Components/Card";
 import { useState, useEffect } from "react";
-import Form from "./Form";
+import Form from "./Components/Form";
 
 const App = () => {
 
